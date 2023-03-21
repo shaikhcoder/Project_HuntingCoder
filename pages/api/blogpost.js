70f17ddf105c7f1168dc9f9c967b673c5f7ad393
ahtisham <shaikhahtisham760@gmail.com>
2023-03-22 02:06:26 +0500

    "just create endpoints"

--- pages/api/blogpost.js
+++ pages/api/blogpost.js
@@ -1,7 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import data from "../../data/data.json"
 
+import * as fs from 'fs'
 
 export default function handler(req, res) {
-  res.status(200).json(data)
+fs.readdir(`data`,(err,data)=>{
+
+if(err){
+return res.status(404).json({"Data":"Data Not Found"})
 }
+return res.status(200).json(data)
+
+})
+  
+}
+// getblog?slug=flask

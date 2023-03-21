420491b4a64380c34653d33f0c01ecd4323a86fe
ahtisham <shaikhahtisham760@gmail.com>
2023-03-22 02:08:18 +0500

    "Added"

---
+++ pages/api/getblog.js
@@ -0,0 +1,14 @@
+
+import * as fs from 'fs'
+
+export default function handler(req, res) {
+fs.readFile(`data/${req.query.slug}.json`,'utf-8',(err,data)=>{
+
+if(err){
+return res.status(404).json({"Data":"Data Not Found"})
+}
+return res.status(200).json(JSON.parse(data))
+
+})
+  
+}

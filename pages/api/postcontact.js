e4f78eb632f264366a14fd33521fbcf942b7b56d
ahtisham <shaikhahtisham760@gmail.com>
2023-03-30 00:55:49 +0500

    add more changes

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -1,12 +1,12 @@
+ 
 import * as fs from 'fs'
-export default function Postcontact(req,res){
-let contactNumber = 0;
+export default async function Postcontact(req,res){
 
 if(req.method === 'POST'){
+let data = await fs.promises.readdir("contactus")
+fs.promises.writeFile(`contactus/${data.length}.json`,JSON.stringify(req.body))
 console.log(req.body)
-fs.writeFile(`contactus/${contactNumber}.json`,JSON.stringify(req.body),(err)=>{
-})
-contactNumber++;
+
 return res.status(200).json(req.body)
 }
 else{
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,62 @@
+import { useState } from "react"
+import style from ".././styles/Contact.module.css"
 export default function Contact (){
+const [email,setEmail] = useState('')
+const [named,setNamed] = useState('')
+const [Phone,setPhone] = useState('')
+const [commit,setcommit] = useState('')
 
-return <div>Hello world</div>
+
+const handleSubmit =(e)=>{
+
+console.log(email,named,Phone,commit)
+
+const data = {named,email,Phone,commit}
+
+fetch('http://localhost:3000/api/postcontact/',{method:"POST",
+headers:{'Content-Type':'application/json',},
+body:JSON.stringify(data)
+}).then(res => res.json())
+.then(data=>{
+alert("Thank You for contacting US")
+setEmail("")
+setNamed("")
+setPhone("")
+setcommit("")
+console.log('Success',data)}).catch((err)=>console.log("ERROR",err))
+
+
+
+e.preventDefault()
+
+}
+
+
+
+return <div className={style.container}>
+<h1>COntact Us</h1>
+<form onSubmit={handleSubmit}>
+<div className="mb-3">
+    <label htmlFor="exampleInputName" className="form-label">Email Your Name</label>
+    <input type="text" value={named} onChange={(e)=>setNamed(e.target.value)} className="form-control" name="name" id="exampleInputName" aria-describedby="Name" />
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Phone</label>
+    <input type="number" value={Phone} onChange={(e)=>setPhone(e.target.value)} className="form-control" id="exampleInputPassword1" />
+  </div>
+  <div className="mb-3">
+<label htmlFor="desc" >Elaborate your concern </label>
+    <textarea className="form-control" value={commit} onChange={(e)=>setcommit(e.target.value)} placeholder="Leave a comment here" id="desc"></textarea>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+</div>
 }
